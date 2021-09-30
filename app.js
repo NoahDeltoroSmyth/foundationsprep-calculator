136f8dc106ce6ae8e6c8d44f1eacf642e1ec6934
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-09-30 14:23:07 -0700

    finished event listener

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+import { add } from "./calculations.js";
+
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
 const addOutput = document.getElementById('add-output');
 
 addButton.addEventListener('click', ()=> {
-    
+    const value1 = Number(addInput1.value);
+    const value2 = Number(addInput2.value);
+    const result = add(value1, value2);
+    console.log(result);
 });
