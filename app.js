fd30cff968d26fa4649d93e2c9b097c7bac9b3ce
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-09-30 15:40:45 -0700

    created subtract javascript

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { add, sub} from "./calculations.js";
+import { add, sub} from './calculations.js';
 
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
@@ -10,4 +10,16 @@ addButton.addEventListener('click', ()=> {
     const value2 = Number(addInput2.value);
     const result = add(value1, value2);
     addOutput.textContent = result;
+});
+
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+const subOutput = document.getElementById('sub-output');
+
+subButton.addEventListener('click', () => {
+    const value1 = Number(subInput1.value);
+    const value2 = Number(subInput2.value);
+    const result = sub(value1, value2);
+    subOutput.textContent = result;
 });
