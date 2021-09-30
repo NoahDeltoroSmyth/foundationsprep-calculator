0a4156883db1637db3a8a7fa5eb2dc8b26d41194
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-09-30 14:32:25 -0700

    finalized the add eventlistener

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ addButton.addEventListener('click', ()=> {
     const value1 = Number(addInput1.value);
     const value2 = Number(addInput2.value);
     const result = add(value1, value2);
-    console.log(result);
+    addOutput.textContent = result;
 });
